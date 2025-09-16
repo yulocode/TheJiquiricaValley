d7747f6f9c15c821720b663a0c02c5c31fb76655
yulocode <yulocode@gmail.com>
2025-09-16 20:46:33 -0300

    main

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import Gaara from './assets/gaara-naruto.gif'
 import './App.css'
+import LoginPage from './LoginPage.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <div class="top">
+        <LoginPage />
+      </div>
       <div>
         <img src={Gaara} className="logo" alt="Gaara GIF" />
       </div>
---
+++ src/LoginPage.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle login logic here
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
